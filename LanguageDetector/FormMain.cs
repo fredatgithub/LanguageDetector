c5dd2184c19f35fb9ec70f6449859513830d2faa
fred <domaine75@gmail.com>
2018-11-07 22:29:17 +0100

    Add methods to count words

--- LanguageDetector/FormMain.cs
+++ LanguageDetector/FormMain.cs
@@ -780,5 +780,47 @@ namespace LanguageDetector
     {
       return sampleDico.OrderByDescending(key => key.Value);
     }
+
+    private static int CountWords(string phrase)
+    {
+      var dico = new Dictionary<string, int>();
+      foreach (var word in SplitWords(phrase))
+      {
+        if (dico.ContainsKey(word.Key))
+        {
+          dico[word.Key]++;
+        }
+        else
+        {
+          dico.Add(word.Key, 1);
+        }
+      }
+
+      return dico.Count;
+    }
+
+    private static Dictionary<string, int> SplitWords(string phrase)
+    {
+      var listOfWords = phrase.Split(' ');
+      var dico = new Dictionary<string, int>();
+      foreach (string word in listOfWords)
+      {
+        if (word.Trim() == string.Empty)
+        {
+          continue;
+        }
+
+        if (dico.ContainsKey(word))
+        {
+          dico[word]++;
+        }
+        else
+        {
+          dico.Add(word, 1);
+        }
+      }
+
+      return dico;
+    }
   }
 }
