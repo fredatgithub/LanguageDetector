5b3c54e1ca8f7b0e4af06d227e606125d45523e3
Fred <domaine75@gmail.com>
2018-11-19 21:06:21 +0100

    Add basic AI methods

--- AIOne/AIHelper.cs
+++ AIOne/AIHelper.cs
@@ -98,7 +98,8 @@ namespace AIOne
             if (!File.Exists(Properties.Settings.Default.FrenchFileName))
             {
               // create french basic file if not exist
-
+              CreateFile(Properties.Settings.Default.FrenchFileName);
+              AddWordsToFile(Properties.Settings.Default.FrenchFileName, GetBasicWords(Properties.Settings.Default.FrenchFileName));
             }
             break;
 
@@ -106,13 +107,64 @@ namespace AIOne
             if (!File.Exists(Properties.Settings.Default.EnglishFileName))
             {
               // create English basic file if not exist
-
+              CreateFile(Properties.Settings.Default.EnglishFileName);
+              AddWordsToFile(Properties.Settings.Default.EnglishFileName, GetBasicWords(Properties.Settings.Default.EnglishFileName));
             }
             break;
-
         }
       }
       
     }
+
+    public static void CreateFile(string fileName)
+    {
+      try
+      {
+          using(StreamWriter sw = new StreamWriter(fileName))
+          {
+
+          }
+      }
+      catch (System.Exception)
+      {
+        // file cannot be created            
+      }
+    }
+
+    public static string GetBasicWords(string fileName)
+    {
+      string result = string.Empty;
+      string language = fileName.SubString(0, fileName.Count - 4);
+      switch (language)
+        {
+          case "french":
+            result = "je,tu,il";
+            break;
+
+          case "english":
+            result = "the,of,and,to,a,in,for,is,on,that,by,this,with,i,you,it,not,or,be,are,from,at,as,your,all,have,new,more,an,was,we,will,home,can,us,about,if,page,my,has,search,free,but,our,one,other,do,no,information,time,they,site,he,up,may,what,which,their,news,out,use,any,there,see,only,so,his,when,contact,here,business,who,web,also,now,help,get,pm,view,first,am,been,would,how,were,me,services,some,these,its,like,service,than,find";
+            break;
+        }
+
+        return result;
+    }
+
+     public static string AddWordsToFile(string fileName, string words)
+     {
+       try
+      {
+          using(StreamWriter sw = new StreamWriter(fileName))
+          {
+            foreach(string word in words.Split(','))
+            {
+              sw.WriteLine(word);
+            }
+          }
+      }
+      catch (System.Exception)
+      {
+        // cannot write file
+      }
+     }
   }
 }
