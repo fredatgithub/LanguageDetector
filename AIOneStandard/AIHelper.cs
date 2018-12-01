9deda4d55dc4d02b394bb850a9ca57df51dd6fdb
fred <domaine75@gmail.com>
2018-12-01 14:26:44 +0100

    Add NuGet project AIOne standard

---
+++ AIOneStandard/AIHelper.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AIOneStandard
+{
+  public static class AiHelper
+  {
+    public static IEnumerable<KeyValuePair<string, int>> SortDicoByValue(Dictionary<string, int> sampleDico)
+    {
+      return sampleDico.OrderByDescending(sortingBy => sortingBy.Value);
+    }
+  }
+}
