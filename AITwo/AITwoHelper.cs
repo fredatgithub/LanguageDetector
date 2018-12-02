8153cfe7086a685ca7c924e4e91e6bbe1119aa85
fred <domaine75@gmail.com>
2018-12-02 22:10:33 +0100

    Add AITwo project

---
+++ AITwo/AITwoHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace AITwo
+{
+  // ReSharper disable once InconsistentNaming
+  public static class AITwoHelper
+  {
+    public static void SentenceSeparation()
+    {
+      
+    }
+  }
+}
