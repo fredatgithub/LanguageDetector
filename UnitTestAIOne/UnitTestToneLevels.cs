c6ba68a447da903b3a9410e99eab6d1a78b48fe5
Fred <domaine75@gmail.com>
2019-02-04 19:12:47 +0100

    Add unit tests

--- UnitTestAIOne/UnitTestToneLevels.cs
+++ UnitTestAIOne/UnitTestToneLevels.cs
@@ -135,7 +135,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-     [TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_Monotony()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Monotony;
@@ -144,7 +144,238 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
+    [TestMethod]
+    public void TestMethod_ToneLevels_Antagonism()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Antagonism;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
 
+    [TestMethod]
+    public void TestMethod_ToneLevels_Hostility()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Hostility;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
 
+[TestMethod]
+    public void TestMethod_ToneLevels_Pain()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Pain;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+[TestMethod]
+    public void TestMethod_ToneLevels_Anger()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Anger;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+[TestMethod]
+    public void TestMethod_ToneLevels_Hate()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Hate;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+[TestMethod]
+    public void TestMethod_ToneLevels_Resentment()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Resentment;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+[TestMethod]
+    public void TestMethod_ToneLevels_NoSympathy()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.NoSympathy;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+[TestMethod]
+    public void TestMethod_ToneLevels_UnexpressedResentment()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.UnexpressedResentment;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+[TestMethod]
+    public void TestMethod_ToneLevels_CovertHostility()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.CovertHostility;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+[TestMethod]
+    public void TestMethod_ToneLevels_Anxiety()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Anxiety;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+[TestMethod]
+    public void TestMethod_ToneLevels_Fear()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Fear;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+[TestMethod]
+    public void TestMethod_ToneLevels_Despair()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Despair;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+[TestMethod]
+    public void TestMethod_ToneLevels_Terror()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Terror;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+[TestMethod]
+    public void TestMethod_ToneLevels_Numb()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Numb;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+[TestMethod]
+    public void TestMethod_ToneLevels_Sympathy()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Sympathy;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+[TestMethod]
+    public void TestMethod_ToneLevels_Propitiation()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Propitiation;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+[TestMethod]
+    public void TestMethod_ToneLevels_Grief()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Grief;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+[TestMethod]
+    public void TestMethod_ToneLevels_MakingAmends()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.MakingAmends;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+[TestMethod]
+    public void TestMethod_ToneLevels_Undeserving()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Undeserving;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+[TestMethod]
+    public void TestMethod_ToneLevels_SelfAbasement()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.SelfAbasement;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+[TestMethod]
+    public void TestMethod_ToneLevels_Victim()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Victim;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+[TestMethod]
+    public void TestMethod_ToneLevels_Hopeless()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Hopeless;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+[TestMethod]
+    public void TestMethod_ToneLevels_Apathy()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Apathy;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+[TestMethod]
+    public void TestMethod_ToneLevels_Useless()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Useless;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+[TestMethod]
+    public void TestMethod_ToneLevels_Dying()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Dying;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+[TestMethod]
+    public void TestMethod_ToneLevels_BodyDeath()
+    {
+      const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.BodyDeath;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
   }
 }
