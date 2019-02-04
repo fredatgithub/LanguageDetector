b7f4dc85aa48a8ff2c8832caa73cff1c77a23be0
Fred <domaine75@gmail.com>
2019-02-04 20:40:37 +0100

    Add several unit tests

--- UnitTestAIOne/UnitTestToneLevels.cs
+++ UnitTestAIOne/UnitTestToneLevels.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using AIOne;
+﻿using AIOne;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestAIOne
@@ -9,6 +7,9 @@ namespace UnitTestAIOne
   public class UnitTestToneLevels
   {
     // TODO write them all in English, then french then as a integer
+
+    #region EnglishTests
+
     [TestMethod]
     public void TestMethod_ToneLevels_Serenity_of_beingness()
     {
@@ -72,7 +73,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-     [TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_Cheerfulness()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Cheerfulness;
@@ -81,7 +82,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-     [TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_StrongInterest()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.StrongInterest;
@@ -90,7 +91,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-     [TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_Conservatism()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Conservatism;
@@ -99,7 +100,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-     [TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_MildInterest()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.MildInterest;
@@ -108,7 +109,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-     [TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_Contented()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Contented;
@@ -117,7 +118,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-     [TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_Disinterested()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Disinterested;
@@ -126,7 +127,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-     [TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_Boredom()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Boredom;
@@ -162,7 +163,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-[TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_Pain()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Pain;
@@ -171,7 +172,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-[TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_Anger()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Anger;
@@ -180,7 +181,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-[TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_Hate()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Hate;
@@ -189,7 +190,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-[TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_Resentment()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Resentment;
@@ -198,7 +199,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-[TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_NoSympathy()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.NoSympathy;
@@ -207,7 +208,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-[TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_UnexpressedResentment()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.UnexpressedResentment;
@@ -216,7 +217,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-[TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_CovertHostility()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.CovertHostility;
@@ -225,7 +226,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-[TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_Anxiety()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Anxiety;
@@ -234,7 +235,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-[TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_Fear()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Fear;
@@ -243,7 +244,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-[TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_Despair()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Despair;
@@ -252,7 +253,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-[TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_Terror()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Terror;
@@ -261,7 +262,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-[TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_Numb()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Numb;
@@ -270,7 +271,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-[TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_Sympathy()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Sympathy;
@@ -279,7 +280,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-[TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_Propitiation()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Propitiation;
@@ -288,7 +289,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-[TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_Grief()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Grief;
@@ -297,7 +298,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-[TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_MakingAmends()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.MakingAmends;
@@ -306,7 +307,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-[TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_Undeserving()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Undeserving;
@@ -315,7 +316,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-[TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_SelfAbasement()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.SelfAbasement;
@@ -324,7 +325,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-[TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_Victim()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Victim;
@@ -333,7 +334,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-[TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_Hopeless()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Hopeless;
@@ -342,7 +343,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-[TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_Apathy()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Apathy;
@@ -351,7 +352,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-[TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_Useless()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Useless;
@@ -360,7 +361,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-[TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_Dying()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.Dying;
@@ -369,7 +370,7 @@ namespace UnitTestAIOne
       Assert.AreEqual(result, expected);
     }
 
-[TestMethod]
+    [TestMethod]
     public void TestMethod_ToneLevels_BodyDeath()
     {
       const AiEnumerations.ToneLevelEn Source = AiEnumerations.ToneLevelEn.BodyDeath;
@@ -377,5 +378,380 @@ namespace UnitTestAIOne
       bool result = AIHelper.IsUpTone(Source);
       Assert.AreEqual(result, expected);
     }
+
+    #endregion
+
+    #region Tone level by number
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_by_number_SerenityOfBeingness_40()
+    {
+      const double Source = 40.0;
+      const bool expected = true;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_by_number_Postulates_30()
+    {
+      const double Source = 30.0;
+      const bool expected = true;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+    
+    [TestMethod]
+    public void TestMethod_ToneLevels_Games_22()
+    {
+      const double Source = 22.0;
+      const bool expected = true;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Action_20()
+    {
+      const double Source = 20.0;
+      const bool expected = true;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Exhilaration_8()
+    {
+      const double Source = 8.0;
+      const bool expected = true;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Aesthetic_6()
+    {
+      const double Source = 6.0;
+      const bool expected = true;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Enthusiasm_4()
+    {
+      const double Source = 4.0;
+      const bool expected = true;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Cheerfulness_3_5()
+    {
+      const double Source = 3.5;
+      const bool expected = true;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_StrongInterest_3_3()
+    {
+      const double Source = 3.3;
+      const bool expected = true;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Conservatism_3()
+    {
+      const double Source = 3.5;
+      const bool expected = true;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_MildInterest_2_9()
+    {
+      const double Source = 2.9;
+      const bool expected = true;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Contented_2_8()
+    {
+      const double Source = 2.8;
+      const bool expected = true;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Disinterested_2_6()
+    {
+      const double Source = 2.6;
+      const bool expected = true;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Boredom_2_5()
+    {
+      const double Source = 2.5;
+      const bool expected = true;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Monotony_2_4()
+    {
+      const double Source = 2.4;
+      const bool expected = true;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Antagonism_2()
+    {
+      const double Source = 2.0;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Hostility_1_9()
+    {
+      const double Source = 1.9;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Pain_1_8()
+    {
+      const double Source = 1.8;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Anger_1_5()
+    {
+      const double Source = 1.5;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Hate_1_4()
+    {
+      const double Source = 1.4;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Resentment_1_3()
+    {
+      const double Source = 1.3;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_NoSympathy_1_2()
+    {
+      const double Source = 1.2;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_UnexpressedResentment_1_15()
+    {
+      const double Source = 1.15;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_CovertHostility_1_1()
+    {
+      const double Source = 1.1;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Anxiety_1_02()
+    {
+      const double Source = 1.02;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Fear_1()
+    {
+      const double Source = 1.0;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Despair_0_98()
+    {
+      const double Source = 0.98;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Terror_0_96()
+    {
+      const double Source = 0.96;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Numb_0_94()
+    {
+      const double Source = 0.94;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Sympathy_0_9()
+    {
+      const double Source = 0.9;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Propitiation_0_8()
+    {
+      const double Source = 0.8;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Grief_0_5()
+    {
+      const double Source = 0.5;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_MakingAmends_0_375()
+    {
+      const double Source = 0.375;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Undeserving_0_3()
+    {
+      const double Source = 0.3;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_SelfAbasement_0_2()
+    {
+      const double Source = 0.2;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Victim_0_1()
+    {
+      const double Source = 0.1;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Hopeless_0_07()
+    {
+      const double Source = 0.07;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Apathy_0_05()
+    {
+      const double Source = 0.05;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Useless_0_03()
+    {
+      const double Source = 0.03;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_Dying_0_01()
+    {
+      const double Source = 0.01;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_ToneLevels_BodyDeath_0()
+    {
+      const double Source = 0;
+      const bool expected = false;
+      bool result = AIHelper.IsUpTone(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    #endregion
   }
 }
