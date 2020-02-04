2af4310682c6334f31d4c7fb11bcd97b520dc25e
Fred <domaine75@gmail.com>
2020-02-05 00:03:43 +0100

    add unit tests

--- UnitTestAIOne/UnitTestRemoveWrongCharacters.cs
+++ UnitTestAIOne/UnitTestRemoveWrongCharacters.cs
@@ -7,10 +7,28 @@ namespace UnitTestAIOne
   public class UnitTestRemoveWrongCharacters
   {
     [TestMethod]
-    public void TestMethod_UnitTestRemoveWrongCharacters_one_word()
+    public void TestMethod_UnitTestRemoveWrongCharacters_one_dash()
     {
       const string Source = "twenty-eight";
       string expected = "twentyeight";
+      string result = AIHelper.RemoveWrongCharacters(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_UnitTestRemoveWrongCharacters_one_space()
+    {
+      const string Source = "twenty nine";
+      string expected = "twenty,nine";
+      string result = AIHelper.RemoveWrongCharacters(Source);
+      Assert.AreEqual(result, expected);
+    }
+
+    [TestMethod]
+    public void TestMethod_UnitTestRemoveWrongCharacters_space_and_period()
+    {
+      const string Source = "one place. One time. One job";
+      string expected = "one,place,One,time,One,job";
       string result = AIHelper.RemoveWrongCharacters(Source);
       Assert.AreEqual(result, expected);
     }
